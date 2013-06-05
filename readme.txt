55a909fcad1b84674fa588223be0a422b688c637
Scott Evans <git@scott.ee>
2013-06-05 12:52:16 +0100

    1.0
    Initial release

---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== WP Instagram Widget ===
+Contributors: scottsweb, codeforthepeople, johnbillion, s1m0nd, simonwheatley
+Donate link: https://flattr.com/profile/scottsweb/things
+Tags: instagram, widget, photos, photography, hipster, sidebar, widgets, simple
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WP Instagram widget is a very no fuss WordPress widget to showcase your latest Instagram pics.
+
+== Description ==
+
+WP Instagram widget is a no fuss WordPress widget to showcase your latest Instagram pics. It does not require you to provide your login details or sign in via oAuth. 
+
+The widget is built with the following philosophy:
+
+* Use sensible and simple markup
+* Provide no styles/css - it is up to you to style the widget to your theme and taste
+* Cache where possible - filters are provided to adjust cache timings 
+* Require little setup - avoid oAuth for example
+* Work standalone and as a drop in to the [null theme framework](https://github.com/scottsweb/null)
+
+== Installation ==
+
+To install this plugin:
+
+1. Upload the `wp-instagram-widget` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. That's it!
+
+To use within the [null theme framework](https://github.com/scottsweb/null) or [null child theme](https://github.com/scottsweb/null-child)
+
+1. Drop the wp-instagram-widget.php file into your /assets/inc/widgets/ folder
+1. Activate via 'Appearance' -> 'Theme Options' menu, under the 'WordPress' tab
+
+== Frequently Asked Questions ==
+
+= Hooks & Filters =
+
+The plugin has one filter that allows you adjust that cache time for retrieving the images from Instagram:
+
+`add_filter('null_instagram_cache_time', 'my_cache_time');
+
+function my_cache_time() {
+	return HOUR_IN_SECONDS;
+}
+`
+
+== Screenshots ==
+
+1. Instagram widget on the widgets wp-admin screen.
+2. Instagram widget on the front end
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ wp-instagram-widget.php
@@ -0,0 +1,199 @@
+<?php
+/*	
+Plugin Name: WP Instagram Widget
+Plugin URI: https://github.com/cftp/wp-instagram-widget
+Description: A WordPress widget for showing your latest Instagram photos
+Version: 1.0
+Author: Code For The People
+Author URI: http://codeforthepeople.com
+Text Domain: wpiw
+Domain Path: /assets/languages/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This comment is added for compatibility with the null framework https://github.com/scottsweb/null
+Widget Name: Instagram Widget
+
+Copyright © 2013 Code for the People ltd
+
+                _____________
+               /      ____   \
+         _____/       \   \   \
+        /\    \        \___\   \
+       /  \    \                \
+      /   /    /          _______\
+     /   /    /          \       /
+    /   /    /            \     /
+    \   \    \ _____    ___\   /
+     \   \    /\    \  /       \
+      \   \  /  \____\/    _____\
+       \   \/        /    /    / \
+        \           /____/    /___\
+         \                        /
+          \______________________/
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+	
+// load is_plugin_active function if required
+if (!function_exists('is_plugin_inactive')) require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+
+// registers widget when bundled with the null framework
+if (is_plugin_inactive('wp-instagram-widget/wordpress-instagram-widget.php') && function_exists('null_get_extensions')) {
+	
+	// register
+	register_widget('null_instagram_widget');
+
+	// text domain
+	$wpiwdomain = 'null';
+}
+
+// register widget for standalone
+if (is_plugin_active('wp-instagram-widget/wp-instagram-widget.php')) {
+
+	// define some constants
+	define('WP_INSTAGRAM_WIDGET_JS_URL',plugins_url('/assets/js',__FILE__));
+	define('WP_INSTAGRAM_WIDGET_CSS_URL',plugins_url('/assets/css',__FILE__));
+	define('WP_INSTAGRAM_WIDGET_IMAGES_URL',plugins_url('/assets/images',__FILE__));
+	define('WP_INSTAGRAM_WIDGET_PATH', dirname(__FILE__));
+	define('WP_INSTAGRAM_WIDGET_BASE', plugin_basename(__FILE__));
+	define('WP_INSTAGRAM_WIDGET_FILE', __FILE__);
+
+	// text domain
+	$wpiwdomain = 'wpiw';
+
+	// load language files
+	load_plugin_textdomain($wpiwdomain, false, dirname(WP_INSTAGRAM_WIDGET_BASE) . '/assets/languages/');
+
+	// register
+	add_action('widgets_init', 'wpiw_widget');
+}
+
+function wpiw_widget() {
+	register_widget('null_instagram_widget');
+}
+
+class null_instagram_widget extends WP_Widget {
+	
+	function null_instagram_widget() {
+		global $wpiwdomain;
+		$this->wpiwdomain = $wpiwdomain;
+		$widget_ops = array('classname' => 'null-instagram-feed', 'description' => __('Displays your latest Instagram photos', $this->wpiwdomain) );
+		$this->WP_Widget('null-instagram-feed', __('Instagram', $this->wpiwdomain), $widget_ops);	
+	}
+
+	function widget($args, $instance) {
+		
+		extract($args, EXTR_SKIP);
+		
+		$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);	
+		$username = empty($instance['username']) ? '' : $instance['username'];
+		$limit = empty($instance['number']) ? 9 : $instance['number'];
+		$size = empty($instance['size']) ? 'thumbnail' : $instance['size'];
+		$link = empty($instance['link']) ? '' : $instance['link'];
+		
+		echo $before_widget;
+		if(!empty($title)) { echo $before_title . $title . $after_title; };
+		
+		if ($username != '') {
+			$images_array = $this->scrape_instagram($username, $limit);
+
+			if ( is_wp_error($images_array) ) {
+			   echo $images_array->get_error_message();
+			} else {
+				?><ul class="instagram-pics"><?php
+				foreach ($images_array as $image) {
+					echo '<li><a href="'.$image['link'].'"><img src="'.$image[$size]['url'].'"  alt="'.$image['description'].'" title="'.$image['description'].'"/></a></li>';
+				}
+				?></ul><?php
+			}
+		}
+
+		if ($link != '') {
+			?><p class="clear"><a href="http://instagram.com/<?php echo $username; ?>"><?php echo $link; ?></a></p><?php  
+		}
+
+		echo $after_widget; 
+	}
+
+	function form($instance) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => __('Instagram', $this->wpiwdomain), 'username' => '', 'link' => __('Follow Us', $this->wpiwdomain), 'number' => 9, 'size' => 'thumbnail') );
+		$title = esc_attr($instance['title']);
+		$username = esc_attr($instance['username']);
+		$number = absint($instance['number']);
+		$size = esc_attr($instance['size']);
+		$link = esc_attr($instance['link']);
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', $this->wpiwdomain); ?>: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('username'); ?>"><?php _e('Username', $this->wpiwdomain); ?>: <input class="widefat" id="<?php echo $this->get_field_id('username'); ?>" name="<?php echo $this->get_field_name('username'); ?>" type="text" value="<?php echo $username; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of Photos', $this->wpiwdomain); ?>: <input class="widefat" id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('size'); ?>"><?php _e('Photo Size', $this->wpiwdomain); ?>:</label>
+			<select id="<?php echo $this->get_field_id('size'); ?>" name="<?php echo $this->get_field_name('size'); ?>" class="widefat">
+				<option value="thumbnail" <?php selected('thumbnail', $size) ?>><?php _e('Thumbnail', $this->wpiwdomain); ?></option>
+				<option value="large" <?php selected('large', $size) ?>><?php _e('Large', $this->wpiwdomain); ?></option>
+			</select>
+		</p>
+		<p><label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Link Text', $this->wpiwdomain); ?>: <input class="widefat" id="<?php echo $this->get_field_id('link'); ?>" name="<?php echo $this->get_field_name('link'); ?>" type="text" value="<?php echo $link; ?>" /></label></p>
+		<?php
+
+	}
+
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['username'] = strip_tags($new_instance['username']);
+		$instance['number'] = !absint($new_instance['number']) ? 9 : $new_instance['number'];
+		$instance['size'] = (($new_instance['size'] == 'thumbnail' || $new_instance['size'] == 'large') ? $new_instance['size'] : 'thumbnail');
+		$instance['link'] = strip_tags($new_instance['link']);
+		return $instance;
+	}
+
+	// based on https://gist.github.com/cosmocatalano/4544576
+	function scrape_instagram($username, $slice = 9) {
+
+		if (false === ($instagram = get_transient('instagram-pics-'.sanitize_title_with_dashes($username)))) {
+			
+			$insta_source = file_get_contents('http://instagram.com/'.$username);
+			$remote = wp_remote_get('http://instagram.com/'.$username);
+
+			if (is_wp_error($remote)) 
+	  			return new WP_Error('site_down', __('Unable to communicate with instagram.', $this->wpiwdomain));
+
+			$shards = explode('"bootstrap",[', $remote['body']);
+			$insta_json = explode('}]]', $shards[1]);
+			$insta_array = json_decode($insta_json[0].'}', TRUE);
+
+			if (!$insta_array)
+	  			return new WP_Error('bad_json', __('Instagram has returned invalid data.', $this->wpiwdomain));
+
+			$images = $insta_array['props']['userMedia'];
+
+			$instagram = array();
+			foreach ($images as $image) {
+				$instagram[] = array(
+					'description' 	=> $image['caption']['text'],
+					'link' 			=> $image['link'],
+					'time'			=> $image['created_time'],
+					'comments' 		=> $image['comments']['count'],
+					'likes' 		=> $image['likes']['count'],
+					'thumbnail' 	=> $image['images']['thumbnail'],
+					'large' 		=> $image['images']['standard_resolution']
+				);
+			}
+			set_transient('instagram-pics-'.sanitize_title_with_dashes($username), $instagram, apply_filters('null_instagram_cache_time', HOUR_IN_SECONDS*2));
+		}
+
+		return array_slice($instagram, 0, $slice);
+	}
+}
+?>
