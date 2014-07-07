9c3f2ad4ade8572606f4af48d0eaad939fad1b12
Scott Evans <git@scott.ee>
2014-07-07 13:10:58 +0100

    Pass widget instance into hooks

--- wp-instagram-widget.php
+++ wp-instagram-widget.php
@@ -105,7 +105,7 @@ class null_instagram_widget extends WP_Widget {
 		echo $before_widget;
 		if(!empty($title)) { echo $before_title . $title . $after_title; };
 
-		do_action( 'wpiw_before_widget' );
+		do_action( 'wpiw_before_widget', $instance );
 
 		if ($username != '') {
 			$images_array = $this->scrape_instagram($username, $limit);
@@ -125,7 +125,7 @@ class null_instagram_widget extends WP_Widget {
 			?><p class="clear"><a href="//instagram.com/<?php echo trim($username); ?>" rel="me"><?php echo $link; ?></a></p><?php
 		}
 
-		do_action( 'wpiw_before_widget' );
+		do_action( 'wpiw_before_widget', $instance );
 
 		echo $after_widget;
 	}
